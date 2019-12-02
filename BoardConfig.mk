3620f97a04c9c671f35dedc54d0628ea5c1ea7cb
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-02 20:15:15 +0000

    mata-user 10 QQ1A.191205.017 499 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,14 @@
+DEVICE_PATH := device/essential/mata
+BOARD_VENDOR := essential
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-12-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.power@1.3-service.mata.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/essential/mata/BoardConfigVendor.mk
